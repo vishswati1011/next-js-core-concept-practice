0e45e6a7fc8865043872386fb6c4831747115159
vishswati1011 <sati.v1011@gmail.com>
2024-06-20 18:27:32 +0530

    user page

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,31 +1,9 @@
-import Link from 'next/link';
-import {getUsersByStaticApiRoutes} from '../../../services/getUsersByApiRoutes';
-import DeleteButton from './component/deleteuser';
-export default async function UserStaticAPi() {
+import React from 'react'
 
-    const getUserList = await getUsersByStaticApiRoutes();
-    const users = await getUserList;
-
+export default function page() {
   return (
     <div>
-        <h1> Fetch User data From api routes</h1>
-        {
-            users.map((user,index)=> {
-                return (
-                
-                    <div key={index}>
-                        <span><Link href={`/users/${user.id}`} key={index}>{user.name} </Link> </span> &nbsp; 
-                            <span> <Link href={`/users/${user.id}/update`}>
-                                <button >edit</button></Link>
-                            </span>
-                            <span>
-                                <DeleteButton userId={user.id}  />
-                            </span>
-                        <br/>
-                    </div>
-                )
-            })
-        }
+        <h1>USer page</h1>
     </div>
   )
 }
